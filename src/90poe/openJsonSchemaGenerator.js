f91418232edb8b32e840b11192c074f223fd5230
Shelex <ovr.shevtsov@gmail.com>
2021-07-27 16:34:02 +0300

    chore: update comment regarding json schema gen

--- src/90poe/openJsonSchemaGenerator.js
+++ src/90poe/openJsonSchemaGenerator.js
@@ -3,6 +3,7 @@ const { getTerminal } = require('../helper/terminal');
 /**
  * 90poe internal command
  * to execute json schema generator binary
+ * but could be used with https://github.com/Shelex/json-schema-gen
  */
 exports.openJsonSchemaGenerator = (file) => {
     const terminal = getTerminal();
